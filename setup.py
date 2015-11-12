d330bca498737b4982eddd76633d55f889a86390
Stanislas <stan@sifli>
2015-11-12 13:09:57 +0100

    numpy hack

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from setuptools import setup
 
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+class build_ext(_build_ext):
+    'to install numpy'
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
 def readme():
     with open('README.rst') as f:
         return f.read()
@@ -13,11 +26,14 @@ setup(name='coclust',
       author='XXXX',
       author_email='XXXX',
       license='BSD3',
-      packages=['coclust' ,'coclust/tests', 'coclust/utils'],
-      setup_requires=["numpy","scipy"],
+      packages=['coclust', 'coclust/tests', 'coclust/utils'],
+      setup_requires=["numpy"],
       install_requires=[
-          'numpy',"scipy","nose"
+          'numpy', "scipy", "scikit-learn"
       ],
+      cmdclass={
+          'build_ext': build_ext
+      },
       entry_points={
           'console_scripts': [
               'coclust = coclust.coclust:main',
@@ -26,4 +42,4 @@ setup(name='coclust',
       include_package_data=True,
       zip_safe=False,
       test_suite='nose.collector',
-      tests_require=['nose'],)
+      tests_require=['nose'])
