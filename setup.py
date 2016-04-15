1f653628bc21ef4861db7a98a0055b9cc81c9b22
Francois Role <francois.role@gmail.com>
2016-04-15 10:13:27 +0200

    Changing setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def readme():
         return f.read()
 
 setup(name='coclust',
-      version='0.1.1',
+      version='0.1.3',
       description='coclustering algorithms for data mining',
       long_description=readme(),
       classifiers=['Topic :: Scientific/Engineering :: Information Analysis'],
