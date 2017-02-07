25ae3d32a91ac6a73cfae029a073759957eab7c0
smorbieu <stanislas.morbieu@gmail.com>
2017-02-07 11:56:49 +0100

    doc autosummary

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,8 @@ extensions = [
     'sphinx.ext.mathjax'
 ]
 
+autosummary_generate = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
