7cb889480204f8f6a63c9dbe32676311f9d30fdb
Francois Role <francois.role@gmail.com>
2019-08-15 16:07:39 +0200

    firts parallel implentation for CoclustMod

--- coclust/coclustering/coclust_mod.py
+++ coclust/coclustering/coclust_mod.py
@@ -12,9 +12,72 @@ of a co-clustering algorithm by direct maximization of graph modularity.
 
 import numpy as np
 from sklearn.utils import check_random_state, check_array
+from joblib import Parallel, delayed, effective_n_jobs
 
 from ..initialization import random_init
 from .base_diagonal_coclust import BaseDiagonalCoclust
+
+
+def _fit_single(X, n_clusters, random_state, init, max_iter, tol, y=None):
+        """Perform one run of co-clustering by direct maximization of graph
+        modularity.
+
+        Parameters
+        ----------
+        X : numpy array or scipy sparse matrix, shape=(n_samples, n_features)
+            Matrix to be analyzed
+        """
+        if init is None:
+            W = random_init(n_clusters, X.shape[1], random_state)
+        else:
+            W = np.matrix(init, dtype=float)
+
+        Z = np.zeros((X.shape[0], n_clusters))
+
+        # Compute the modularity matrix
+        row_sums = np.matrix(X.sum(axis=1))
+        col_sums = np.matrix(X.sum(axis=0))
+        N = float(X.sum())
+        indep = (row_sums.dot(col_sums)) / N
+
+        # B is a numpy matrix
+        B = X - indep
+
+        modularities = []
+
+        # Loop
+        m_begin = float("-inf")
+        change = True
+        iteration = 0
+        while change:
+            change = False
+
+            # Reassign rows
+            BW = B.dot(W)
+            for idx, k in enumerate(np.argmax(BW, axis=1)):
+                Z[idx, :] = 0
+                Z[idx, k] = 1
+
+            # Reassign columns
+            BtZ = (B.T).dot(Z)
+            for idx, k in enumerate(np.argmax(BtZ, axis=1)):
+                W[idx, :] = 0
+                W[idx, k] = 1
+
+            k_times_k = (Z.T).dot(BW)
+            m_end = np.trace(k_times_k)
+            iteration += 1
+            if (np.abs(m_end - m_begin) > tol and
+                    iteration < max_iter):
+                modularities.append(m_end/N)
+                m_begin = m_end
+                change = True
+
+        row_labels_ = np.argmax(Z, axis=1).tolist()
+        column_labels_ = np.argmax(W, axis=1).tolist()
+        modularity = m_end / N
+        nb_iterations = iteration
+        return row_labels_,  column_labels_, modularity, modularities, nb_iterations
 
 
 class CoclustMod(BaseDiagonalCoclust):
@@ -66,18 +129,21 @@ class CoclustMod(BaseDiagonalCoclust):
     """
 
     def __init__(self, n_clusters=2, init=None, max_iter=20, n_init=1,
-                 tol=1e-9, random_state=None):
+                 tol=1e-9, random_state=None, n_jobs=1):
         self.n_clusters = n_clusters
         self.init = init
         self.max_iter = max_iter
         self.n_init = n_init
         self.tol = tol
         self.random_state = random_state
+        self.n_jobs = n_jobs
 
         self.row_labels_ = None
         self.column_labels_ = None
         self.modularity = -np.inf
         self.modularities = []
+
+
 
     def fit(self, X, y=None):
         """Perform co-clustering by direct maximization of graph modularity.
@@ -103,110 +169,38 @@ class CoclustMod(BaseDiagonalCoclust):
 
         modularity = self.modularity
         modularities = []
-        row_labels_ = None
-        column_labels_ = None
-
+        row_labels = None
+        column_labels = None
         seeds = random_state.randint(np.iinfo(np.int32).max, size=self.n_init)
-        for seed in seeds:
-            self._fit_single(X, seed, y)
-            if np.isnan(self.modularity):
+        if effective_n_jobs(self.n_jobs) == 1:
+         for seed in seeds:
+            new_row_labels,  new_column_labels, new_modularity, new_modularities, new_nb_iterations = _fit_single(X, self.n_clusters, seed, self.init, self.max_iter, self.tol, y)
+            if np.isnan(new_modularity):
                 raise ValueError("matrix may contain unexpected NaN values")
             # remember attributes corresponding to the best modularity
-            if (self.modularity > modularity):
-                modularity = self.modularity
-                modularities = self.modularities
-                row_labels_ = self.row_labels_
-                column_labels_ = self.column_labels_
+            if (new_modularity > modularity):
+                modularity = new_modularity
+                modularities = new_modularities
+                row_labels = new_row_labels
+                column_labels = new_column_labels
+        else:
+         results = Parallel(n_jobs=self.n_jobs, verbose=0)(
+            delayed(_fit_single)(X, self.n_clusters, seed, self.init, self.max_iter, self.tol, y)
+            for seed in seeds)
+         list_of_row_labels,  list_of_column_labels, list_of_modularity, list_of_modularities, list_of_nb_iterations = zip(*results)
+         best = np.argmin(list_of_modularity)
+         row_labels = list_of_row_labels[best]
+         column_labels = list_of_column_labels[best]
+         modularity = list_of_modularity[best]
+         modularities = list_of_modularities[best]
+         n_iter = list_of_nb_iterations[best]
+
+         
 
         # update attributes
         self.modularity = modularity
         self.modularities = modularities
-        self.row_labels_ = row_labels_
-        self.column_labels_ = column_labels_
+        self.row_labels_ = row_labels
+        self.column_labels_ = column_labels
 
         return self
-
-    def _fit_single(self, X, random_state, y=None):
-        """Perform one run of co-clustering by direct maximization of graph
-        modularity.
-
-        Parameters
-        ----------
-        X : numpy array or scipy sparse matrix, shape=(n_samples, n_features)
-            Matrix to be analyzed
-        """
-
-        if self.init is None:
-            W = random_init(self.n_clusters, X.shape[1], random_state)
-        else:
-            W = np.matrix(self.init, dtype=float)
-
-        Z = np.zeros((X.shape[0], self.n_clusters))
-
-        # Compute the modularity matrix
-        row_sums = np.matrix(X.sum(axis=1))
-        col_sums = np.matrix(X.sum(axis=0))
-        N = float(X.sum())
-        indep = (row_sums.dot(col_sums)) / N
-
-        # B is a numpy matrix
-        B = X - indep
-
-        self.modularities = []
-
-        # Loop
-        m_begin = float("-inf")
-        change = True
-        iteration = 0
-        while change:
-            change = False
-
-            # Reassign rows
-            BW = B.dot(W)
-            for idx, k in enumerate(np.argmax(BW, axis=1)):
-                Z[idx, :] = 0
-                Z[idx, k] = 1
-
-            # Reassign columns
-            BtZ = (B.T).dot(Z)
-            for idx, k in enumerate(np.argmax(BtZ, axis=1)):
-                W[idx, :] = 0
-                W[idx, k] = 1
-
-            k_times_k = (Z.T).dot(BW)
-            m_end = np.trace(k_times_k)
-            iteration += 1
-            if (np.abs(m_end - m_begin) > self.tol and
-                    iteration < self.max_iter):
-                self.modularities.append(m_end/N)
-                m_begin = m_end
-                change = True
-
-        self.row_labels_ = np.argmax(Z, axis=1).tolist()
-        self.column_labels_ = np.argmax(W, axis=1).tolist()
-        self.btz = BtZ
-        self.bw = BW
-        self.modularity = m_end / N
-        self.nb_iterations = iteration
-
-    def get_assignment_matrix(self, kind, i):
-        """Returns the indices of 'best' i cols of an assignment matrix
-        (row or column).
-
-        Parameters
-        ----------
-        kind : string
-             Assignment matrix to be used: rows or cols
-
-        Returns
-        -------
-        numpy array or scipy sparse matrix
-            Matrix containing the i 'best' columns of a row or column
-            assignment matrix
-        """
-        if kind == "rows":
-            s_bw = np.argsort(self.bw)
-            return s_bw[:, -1:-(i+1):-1]
-        if kind == "cols":
-            s_btz = np.argsort(self.btz)
-            return s_btz[:, -1:-(i+1):-1]
